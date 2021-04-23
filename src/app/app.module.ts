fb11f94e6ce3530f627065f129061e5d308febb4
pooja ghodekar <ghodekar.pooja14@gmail.com>
2021-04-23 14:57:59 +0530

    refactore:dev:change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './About/about-us/about-us.component';
+import { HomeComponent } from './Home/home/home.component';
+import { ContactUsComponent } from './Contact/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    HomeComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule
